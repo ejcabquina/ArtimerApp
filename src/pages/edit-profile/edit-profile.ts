0d27a77fcaec7e5b7de32ab52cfe405758d18369
ejcabquina <ejcabquina@gmail.com>
2018-10-11 03:55:11 +0800

    fixed edit profile

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -5,7 +5,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from '../../providers/auth-service/auth-service';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 
